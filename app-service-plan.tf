9a64b998dfd49924d8f1e3d8e16dba3b282a601e
Craig Thacker <craigthackerx@gmail.com>
2022-04-30 18:03:28 +0100

    Update Module

--- app-service-plan.tf
+++ app-service-plan.tf
@@ -5,7 +5,7 @@ resource "azurerm_app_service_plan" "plan" {
   resource_group_name          = var.rg_name
   kind                         = var.kind
   reserved                     = var.kind == "Linux" ? true : var.reserved
-  app_service_environment_id   = var.add_to_app_service_environment ? var.app_service_environment_id : false
+  app_service_environment_id   = var.add_to_app_service_environment == true ? var.app_service_environment_id : null
   maximum_elastic_worker_count = var.maximum_elastic_worker_count
   per_site_scaling             = var.per_site_scaling
   zone_redundant               = var.zone_redundant
